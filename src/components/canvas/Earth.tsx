e3200af2f3c4db82aa2bf7b35028ffd7b620c4fc
Rana718 <ranadolui718@gmail.com>
2024-09-02 16:58:56 +0530

    earth canvas

---
+++ src/components/canvas/Earth.tsx
@@ -0,0 +1,42 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { GLTF } from "three/examples/jsm/loaders/GLTFLoader";
+
+import CanvasLoader from "../Loader";
+
+const Earth: React.FC = () => {
+  const { scene } = useGLTF("./planet/scene.gltf") as GLTF;
+
+  return <primitive object={scene} scale={2.5} position-y={0} rotation-y={0} />;
+};
+
+const EarthCanvas: React.FC = () => {
+  return (
+    <Canvas
+      shadows
+      frameloop='demand'
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Earth />
+        <Preload all />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+export default EarthCanvas;
