e7bb8bec5d6a5d5df8ec3dbba7bde04199a6a9b4
Rana718 <ranadolui718@gmail.com>
2024-09-11 18:54:30 +0530

    add my logo

---
+++ src/assets/index.ts
@@ -0,0 +1,27 @@
+import logo from "./my_logo.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  carrent,
+  jobit,
+  tripguide,
+};
