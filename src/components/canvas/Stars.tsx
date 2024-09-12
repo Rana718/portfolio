e10b1fbcf7022bee5e1d65e0d393f06a61ec15b3
Rana718 <ranadolui718@gmail.com>
2024-09-13 02:29:42 +0530

    fix the error

--- src/components/canvas/Stars.tsx
+++ src/components/canvas/Stars.tsx
@@ -13,7 +13,7 @@ const Stars = (props:StarsProps) => {
   const ref = useRef<Group>(null);
   const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
 
-  useFrame((state, delta) => {
+  useFrame((_, delta) => {
     if (ref.current) {
       ref.current.rotation.x -= delta / 10;
       ref.current.rotation.y -= delta / 15;
