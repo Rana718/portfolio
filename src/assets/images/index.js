29204a271105e2dd6eb3a3ed12823fb7303ff347
Rana718 <ranadolui718@gmail.com>
2025-05-05 16:07:09 +0530

    upgrade

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,11 +2,17 @@ import caree from './careerwise.png';
 import codec from './codeCraft.png';
 import peer from './peerdrop.png';
 import Tic_icon from './tic-tac-toe.png'
+import pinglo from './pinglo.png';
+import megic from './megic.png';
+import urlshort from './urlshort.png';
 
 
 export{
     caree,
     codec,
     peer,
-    Tic_icon
+    Tic_icon,
+    pinglo,
+    megic,
+    urlshort
 }
